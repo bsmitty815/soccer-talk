66526de879694ba5959f1f0b0ae2c67c7a6b9523
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 12:02:44 -0500

    working on a header and app component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import { Switch, Router } from 'react-router-dom'
+import Header from './Components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Header />
+        <Route>
 
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ function Header() {
 
     return (
         <div>
-
+            <h1>Soccer Talk</h1>
         </div>
     )
 }
