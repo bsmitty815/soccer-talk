b281fa2e04f229e40715e8c70019f92f8fc00b7e
Brian Smith <brian.bubbam@gmail.com>
2021-09-17 19:15:17 -0500

    set up user login form

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,19 +1,41 @@
 
+import { useState } from 'react'
 
 function LoginForm() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState([])
 
+    function handleSubmit() {
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Accepts": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password}),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => setUsername(user))
+                setUsername("")
+                setPassword("")
+            } else {
+                r.json().then((err) => setErrors(err.error))
+            }
+        })
+    }
 
     return (
         <div>
             <h1>Login</h1>
-            <form className="ui form">
+            <form className="ui form" onSubmit={handleSubmit}>
             <div className="field">
                 <label>Username</label>
-                <input type="text" name="username" placeholder="Username" />
+                <input type="text" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div className="field">
                 <label>Password</label>
-                <input type="text" name="password" placeholder="Password" />
+                <input type="text" name="password" placeholder="Password" value={password} onchange={(e) => setPassword(e.target.value)} />
             </div>
             <button className="ui button" type="submit">Submit</button>
             </form>
