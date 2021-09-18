a2264185792c8373402da3ff9bdfecf75c46de1d
Brian Smith <brian.bubbam@gmail.com>
2021-09-18 17:16:12 -0500

    working on navbar and routes

--- src/Components/HomePageContainer.js
+++ src/Components/HomePageContainer.js
@@ -1,5 +1,5 @@
 
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import AllDiscussionsContainer from './Discussions/AllDiscussionsContainer'
 import LoginSignupContainer from './Auth/LoginSignupContainer'
 import ProfileContainer from './Profile/ProfileContainer'
@@ -11,9 +11,23 @@ function HomePageContainer({setUser}) {
     return (
         <div className="home-page-container">
             <div className="home-page-left-container">
-            <Route>
-                <AllDiscussionsContainer />
-            </Route>
+            <Switch>
+                <Route to="/Home">
+                    <AllDiscussionsContainer />
+                </Route>
+                <Route to="/EditProfile">
+                    <EditProfile />
+                </Route>
+                <Route to="/EditPasssword">
+                    <EditPassword />
+                </Route>
+                <Route to="/CreateDiscussion">
+                    <CreateDiscussion/>
+                </Route>
+                <Route to="/">
+                    <AllDiscussionsContainer />
+                </Route>
+            </Switch>
             </div>
 
             <div className="home-page-right-container">
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom'
 
 function NavBar({setUser}) {
     //logout/editprofile/editpassword/create discussion
@@ -18,6 +19,18 @@ function NavBar({setUser}) {
     return (
         <div>
             <h1>navbar container</h1>
+            <Link to='/Home'>
+                <button>Home</button>
+            </Link>
+            <Link to='/EditProfile'>
+                <button>Edit Profile</button>
+            </Link>
+            <Link to='/EditPassword'>
+                <button>Edit Password</button>
+            </Link>
+            <Link to='/CreateDiscussion'>
+                <button>Create Discussion</button>
+            </Link>
             <button onClick={handleLogout} >Logout</button>
         </div>
     )
